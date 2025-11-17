2926b1371c124925384211e88e78421e6fae8380
ConfleisX01 <blackstar371@outlook.com>
2025-11-17 03:05:11 -0600

    Nuevo end point para obtencion de informacion del usuario

---
+++ ProjectTakeCareBack/Models/UserInformationDTO.cs
@@ -0,0 +1,18 @@
+﻿using ProjectTakeCareBack.Enums;
+
+namespace ProjectTakeCareBack.Models
+{
+    public class UserInformationDTO
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; } = null!;
+        public string? ApellidoPaterno { get; set; } = null!;
+        public string? ApellidoMaterno { get; set; }
+        public string? Genero { get; set; } = null!;
+        public string? Correo { get; set; } = null!;
+        public string? Telefono { get; set; } = null!;
+        public RolUsuario Rol { get; set; }
+        public Psicologo? Psicologo { get; set; } // Regresar informacion dependiendo si encuentra coincidencias con el rol
+        public Paciente? Paciente { get; set; }
+    }
+}
